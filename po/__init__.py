0e4d53edfc9bd87095bd7d99f8249cfa9a756606
Adam Goucher <adam@goucher.ca>
2011-07-25 10:10:30 -0400

    trying to use a package instead of a module

--- po/__init__.py
+++ po/__init__.py
@@ -1,5 +1,7 @@
 from SeleniumWrapper import SeleniumWrapper as wrapper
-from HomePage import HomePage
+
+from homepage import HomePage
+from signinpage import SignInPage
 
 def open_browser_to_english_home_page():
     se = wrapper().connect("127.0.0.1", "4444", "*firefox", "http://www.workopolis.com")
